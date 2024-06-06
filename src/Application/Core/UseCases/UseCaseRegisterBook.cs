f8be8764840b4b39e19b5c45107c0b1617858ec2
Cickruss <icaro.o.macedo@gmail.com>
2024-06-06 10:12:59 -0300

    feat: implement usecase without validation

--- src/Application/Core/UseCases/UseCaseRegisterBook.cs
+++ src/Application/Core/UseCases/UseCaseRegisterBook.cs
@@ -2,14 +2,29 @@
 using api_cadastro.Application.Domain.Dto.Command;
 using api_cadastro.Application.Domain.DTO.Command;
 using api_cadastro.Application.Ports.Inbound.UseCases;
+using api_cadastro.Application.Ports.Outbound.DB.Repository;
 
 namespace api_cadastro.Application.Core.UseCases
 {
     public class UseCaseRegisterBook : IUseCaseRegisterBook
     {
-        public Task<BaseReturn> Execute(CommandRegisterBook command)
+        private readonly IRegisterBookRepository ?_repository;
+        public UseCaseRegisterBook(IServiceProvider provider)
         {
-            throw new NotImplementedException();
+            _repository = provider.GetService<IRegisterBookRepository>();
+        }
+
+        public async Task<BaseReturn> Execute(CommandRegisterBook command)
+        {
+            try
+            {
+                var response = await _repository!.RegisterBook(command);
+                return new BaseReturn().Success(command);
+            }
+            catch (Exception ex)
+            {
+                return new BaseReturn().SystemError(ex);
+            }
         }
     }
 }
--- src/Application/Core/UseCases/UseCaseRegisterUser.cs
+++ src/Application/Core/UseCases/UseCaseRegisterUser.cs
@@ -1,14 +1,28 @@
 ﻿using api_cadastro.Application.Domain.Dto.Base;
 using api_cadastro.Application.Domain.Dto.Command;
 using api_cadastro.Application.Ports.Inbound.UseCases;
+using api_cadastro.Application.Ports.Outbound.DB.Repository;
 
 namespace api_cadastro.Application.Core.UseCases
 {
     public class UseCaseRegisterUser : IUseCaseRegisterUser
     {
-        public Task<BaseReturn> Execute(CommandRegisterUser transacao)
+        private readonly IRegisterUserRepository ?_repository;
+
+        public UseCaseRegisterUser(IServiceProvider provider)
         {
-            throw new NotImplementedException();
+            _repository = provider.GetService<IRegisterUserRepository>();
+        }
+        public async Task<BaseReturn> Execute(CommandRegisterUser command)
+        {
+            try
+            {
+                var response = await _repository!.RegisterUser(command);
+                return new BaseReturn().Success(response);
+            }catch (Exception ex) 
+            {
+                return new BaseReturn().SystemError(ex);
+            }
         }
     }
 }
